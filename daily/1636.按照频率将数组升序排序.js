85dc4094f6e338d31d6a30dee9be8bf1198a9ae3
chenzhi <chenzhi@matrixelements.com>
2022-10-13 20:34:47 +0800

    feat: 1636

--- daily/1636.按照频率将数组升序排序.js
+++ daily/1636.按照频率将数组升序排序.js
@@ -9,8 +9,26 @@
  * @param {number[]} nums
  * @return {number[]}
  */
-var frequencySort = function(nums) {
+var frequencySort = function (nums) {
+  const countMap = {}
+  nums.forEach((item) => {
+    const count = countMap[item]
+    if (count) {
+      countMap[item] = count + 1
+    } else {
+      countMap[item] = 1
+    }
+  })
+  let arr = Object.keys(countMap).sort((a, b) => {
+    const result = countMap[a] - countMap[b]
+    if (result === 0 && a - b > 0) {
+      return -1
+    }
+    return result
+  })
+  return arr.reduce((a, b) => {
+    return a.concat(new Array(countMap[b]).fill(b))
+  }, [])
+}
 
-};
 // @lc code=end
-
