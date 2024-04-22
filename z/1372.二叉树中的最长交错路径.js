74612bd14e3878edec24e5e1ce1ac09f5d7fe2cf
zfamz <812158392@qq.com>
2024-04-22 17:21:29 +0800

    feat: 240422-1

--- z/1372.二叉树中的最长交错路径.js
+++ z/1372.二叉树中的最长交错路径.js
@@ -17,8 +17,24 @@
  * @param {TreeNode} root
  * @return {number}
  */
-var longestZigZag = function(root) {
-
-};
+var longestZigZag = function (root) {
+  let ans = 0
+  const dfs = (node, isLeft) => {
+    if (!node) return 0
+    let l_count = dfs(node.left, true)
+    let r_count = dfs(node.right, false)
+    // 以当前节点为根节点计算
+    let _ans = Math.max(r_count, l_count) + 1
+    // 返回父节点需要的
+    let res = (isLeft ? r_count : l_count) + 1
+    ans = Math.max(ans, _ans, res)
+    return res
+  }
+  dfs(root)
+  return ans - 1
+}
 // @lc code=end
-
+import { gTree } from '../lib/tree.js'
+// const args = [1, null, 1, 1, 1, null, null, 1, 1, null, 1, null, null, null, 1, null, 1]
+const args = [1, 1, 1, null, 1, null, null, 1, 1, null, 1]
+console.log(longestZigZag(gTree(args)))
