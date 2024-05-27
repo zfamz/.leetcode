aefc597b2113db0c3f579f1bd1c73b4db52a8e6c
zfamz <812158392@qq.com>
2024-05-27 09:06:27 +0800

    feat: daily

--- daily/2028.找出缺失的观测数据.js
+++ daily/2028.找出缺失的观测数据.js
@@ -12,21 +12,34 @@
  * @return {number[]}
  */
 var missingRolls = function (rolls, mean, n) {
-  let sum = rolls.reduce((a, b) => a - b, mean * (rolls.length + n))
-  if (sum <= 6 * n && sum > n) {
-    const ans = Array(n).fill(1)
-    sum -= n
-    for (let i = 0; sum > 0; i++) {
-      if (sum > 5) {
-        ans[i] = 6
-        sum -= 5
-      } else {
-        ans[i] = sum + 1
-        sum = 0
-      }
-    }
-    return ans
+  let sum = mean * (rolls.length + n)
+  for (const r of rolls) {
+    sum -= r
   }
-  return []
+
+  if (sum > n * 6 || sum < n) {
+    return []
+  }
+
+  let base = Math.floor(sum / n)
+  sum = sum - base * n
+  let ans = new Array(n).fill(base)
+  let max = 6 - base
+
+  let i = 0
+  while (sum > 0) {
+    if (sum > max) {
+      ans[i] = 6
+      sum -= max
+    } else {
+      ans[i] = base + sum
+      sum = 0
+    }
+    i++
+  }
+
+  return ans
 }
 // @lc code=end
+const args = [[3, 2, 4, 3], 4, 2]
+console.log(missingRolls(...args))
