7cb40ed4e2a1e1cd1cd86695c22e2d1f0704db28
zfamz <812158392@qq.com>
2024-06-07 21:42:23 +0800

    feat: 0607_2

--- series/01基础算法精讲/05二叉树与递归 - 前序_中序_后序/98.验证二叉搜索树.js
+++ series/01基础算法精讲/05二叉树与递归 - 前序_中序_后序/98.验证二叉搜索树.js
@@ -17,8 +17,56 @@
  * @param {TreeNode} root
  * @return {boolean}
  */
-var isValidBST = function(root) {
+var isValidBST = function (root) {
+  let ans = true
+  const dfs = (node) => {
+    if (!ans || !node) return
+    const { left, right, val } = node
+    let [l_l, l_r] = dfs(left) || [val, val - 1]
+    let [r_l, r_r] = dfs(right) || [val + 1, val]
 
-};
+    if (val > l_r && val < r_l) {
+      return [l_l, r_r]
+    } else {
+      ans = false
+      return
+    }
+  }
+  dfs(root)
+  return ans
+}
 // @lc code=end
+import { gTree } from '../../../lib/tree.js'
+// const args = gTree([2, 2, 2])
+// const args = gTree([5, 1, 4, null, null, 3, 6])
+const args = gTree([2, 1, 3])
+console.log(isValidBST(args))
 
+var isValidBST_in = function (root) {
+  let prev = -Infinity
+  const dfs = (node) => {
+    if (!node) return true
+    const state = dfs(node.left)
+    if (state && node.val > prev) {
+      prev = node.val
+      return dfs(node.right)
+    }
+    return false
+  }
+  return dfs(root)
+}
+
+var isValidBST_prev = function (root) {
+  const dfs = (node, max, min) => {
+    if (!node) return true
+    const { val } = node
+    if (val >= max || val <= min) {
+      return false
+    }
+    const left = dfs(node.left, val, min)
+    const right = dfs(node.right, max, val)
+
+    return left && right
+  }
+  return dfs(root, Infinity, -Infinity)
+}
